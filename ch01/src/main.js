08a4bcdf72b8d3498ead37f71ba930ece6d569d4
Akira Chiku <akira.chiku@gmail.com>
2017-04-22 14:42:22 +0900

    Add webpack-dev-server

--- ch01/src/main.js
+++ ch01/src/main.js
@@ -1,6 +1,6 @@
 function component () {
   var element = document.createElement('div');
-  element.innerHTML = [1, 2, 3].map(x => 'hello' + x)
+  element.innerHTML = [1, 2, 3, 4, 20].map(x => 'hello' + x)
   return element;
 }
 
--- ch01/webpack.config.js
+++ ch01/webpack.config.js
@@ -3,16 +3,22 @@ const path = require('path');
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       { 
-        test: /\.js$/, 
-        exclude: /node_modules/, 
+        test: "/\.js$/",
+        exclude: "/node_modules/", 
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 3355,
+    hot: true,
+    inline: true
+  },
 };
