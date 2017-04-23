7abb2a5c1a98bd48e67623921fe1045b3fd2ffef
Akira Chiku <akira.chiku@gmail.com>
2017-04-23 13:10:36 +0900

    Add ch03

---
+++ ch03/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 3355,
+    hot: true,
+    inline: true
+  },
+};
